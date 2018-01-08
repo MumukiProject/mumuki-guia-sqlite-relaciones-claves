41bb8e2bba1b24ed66727161703d64bab2304795
nadia@mumuki.org <nadia@mumuki.org>
2018-01-08 14:59:37 -0500

    Mumuki Export on 2018-01-08 14:59:37 -0500

---
+++ 00001_Repetí conmigo.../extra.sql
@@ -0,0 +1,7 @@
+CREATE TABLE canciones (id_contenido INTEGER PRIMARY KEY, titulo TEXT, album TEXT, artista TEXT, genero TEXT, anio INTEGER);
+
+  
+INSERT INTO series_peliculas VALUES 
+(1, "Bohemian rhapsody", "A night at the Opera", "Queen", "rock", 1975), 
+(2, "Can`t buy me love", "A hard day`s night", "The Beatles", "rock, pop", 1964),
+(3, "Baby on board", "Más grandes que Jesús", "Los Borbotones", "rock, pop", 1985);
